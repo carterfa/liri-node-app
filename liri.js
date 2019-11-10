f11549372c0d649a300a113c290d66a0066da53e
Farrah Carter <carterfarraha@gmail.com>
2019-11-10 14:40:32 -0500

    Movie search working

--- liri.js
+++ liri.js
@@ -1,33 +1,20 @@
-//Spotify requirements
+//Requirements
 var Spotify = require('node-spotify-api');
 require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+var axios = require('axios');
 
+//grabs variables from command line
 const command = process.argv[2];
-const title = process.argv.slice(3).join(" ");
 
-switch (command) {
-    case "spotify-this-song":
-        songSearch(title);
-        break;
 
-    case "concert-this":
-        console.log("concerttime");
-        break;
+function songSearch() {
 
-    case "movie-this":
-        console.log("movie-search");
-        break;
-
-    case "do-what-it-says":
-        console.log("doing it");
-        break;
-}
-
-function songSearch(title) {
+    let title = process.argv.slice(3).join(" ");
 
     //default if no input
+    console.log(title);
     if (title === "") {
         title = "The Sign Ace of Base";
     }
@@ -53,4 +40,58 @@ function songSearch(title) {
         console.log(error);
     });
 
+}
+
+function movieSearch() {
+
+    let title = process.argv.slice(3).join("+");
+
+    //default if no input
+    console.log(title)
+    if (title === "") {
+        title = "Mr.+Nobody";
+    }
+
+    let queryURL = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
+
+    //console.log(queryURL);
+
+    axios.get(queryURL).then(function (response) {
+        const movie = response.data;
+
+        console.log("Title: " + movie.Title);
+        console.log("Year: " + movie.Year);
+        console.log("imdb Rating: " + movie.imdbRating);
+
+        //only logs rotten tomatoes rating if there is one
+        if (movie.Ratings[1] !== undefined){
+        console.log("Rotten Tomatoes: " + movie.Ratings[1].Value);}
+
+        console.log("Country: " + movie.Country);
+        console.log("Language: " + movie.Language);
+        console.log("Plot: " + movie.Plot);
+        console.log("Actors: " + movie.Actors);
+
+    }).catch(function (error) {
+        console.log(error);
+    });
+
+}
+
+switch (command) {
+    case "spotify-this-song":
+        songSearch();
+        break;
+
+    case "concert-this":
+        console.log("concerttime");
+        break;
+
+    case "movie-this":
+        movieSearch();
+        break;
+
+    case "do-what-it-says":
+        console.log("doing it");
+        break;
 }
