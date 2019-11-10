c7f6423d7aea8259fc27a4f394b71a6373c0b2b6
Farrah Carter <carterfarraha@gmail.com>
2019-11-10 13:50:06 -0500

    Spotify search working

--- liri.js
+++ liri.js
@@ -5,10 +5,11 @@ var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
 const command = process.argv[2];
+const title = process.argv.slice(3).join(" ");
 
 switch (command) {
     case "spotify-this-song":
-        console.log("spotifyme");
+        songSearch(title);
         break;
 
     case "concert-this":
@@ -22,4 +23,34 @@ switch (command) {
     case "do-what-it-says":
         console.log("doing it");
         break;
+}
+
+function songSearch(title) {
+
+    //default if no input
+    if (title === "") {
+        title = "The Sign Ace of Base";
+    }
+
+    spotify.search(
+        {
+            type: 'track',
+            query: title
+        }
+    ).then(function (response) {
+        let data = response.tracks.items[0];
+
+        //console logs all song data
+        //console.log(data);
+        console.log("Artist(s): ");
+        for (let i = 0; i < data.artists.length; i++) {
+            console.log(data.artists[i].name);
+        }
+        console.log("Song Title: " + data.name);
+        console.log("Preview: " + data.preview_url);
+        console.log("Album: " + data.album.name);
+    }).catch(function (error) {
+        console.log(error);
+    });
+
 }
